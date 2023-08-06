0153433b0d10b9f70d7e0f2ca4e00b99138edb1f
dindin <dontemailme@gmail.com>
2023-08-05 20:45:29 -0400

    impl of some of the texture class methods

--- src/texture/texture.cpp
+++ src/texture/texture.cpp
@@ -6,14 +6,20 @@
 #include "texture.hpp"
 #include "window/window.hpp"
 
-MyTexture::~MyTexture() {}
+MyTexture::~MyTexture() { this->free(); }
 
 // set color modulation
-void MyTexture::set_color(uint8_t red, uint8_t green, uint8_t blue){}
+void MyTexture::set_color(uint8_t red, uint8_t green, uint8_t blue){
+    SDL_SetTextureColorMod(this->texture, red, green, blue);
+}
 
 // alpha modulation stuff
-void MyTexture::set_blend_mode(SDL_BlendMode blending){}
-void MyTexture::set_alpha(uint8_t alpha){}
+void MyTexture::set_blend_mode(SDL_BlendMode blending){
+    SDL_SetTextureBlendMode(this->texture, blending);
+}
+void MyTexture::set_alpha(uint8_t alpha){
+    SDL_SetTextureAlphaMod(this->texture, alpha);
+}
 
 void MyTexture::render( int x, int y, SDL_Rect* clip){}
 
